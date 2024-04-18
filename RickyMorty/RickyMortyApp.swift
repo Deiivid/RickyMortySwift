7778215dd99e801abed6c3712ee6cb2df53f0c0c
dnavarro <dnavarro@hiberus.com>
2024-04-18 16:39:58 +0200

    Initial Commit

---
+++ RickyMorty/RickyMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickyMortyApp.swift
+//  RickyMorty
+//
+//  Created by David Navarro Moreno on 18/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RickyMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
